77aca71900d66c9441f6611c6543beb408e6c835
Shintaro Miyazaki <tudopassa427911@gmail.com>
2024-01-29 11:23:45 +0900

    Add Firebase configuration and functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,25 @@
+import 'package:datadog_flutter_plugin/datadog_flutter_plugin.dart';
 import 'package:flutter/material.dart';
 import 'package:the_wall/pages/auth_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 void main() async {
+  final configuration = DatadogConfiguration(
+    clientToken: 'pub3a63fb84a35c7fa2269d949b8ab4c706',
+    env: 'prod',
+    site: DatadogSite.ap1,
+    nativeCrashReportEnabled: true,
+    loggingConfiguration: DatadogLoggingConfiguration(),
+    rumConfiguration: DatadogRumConfiguration(
+      applicationId: '2505fdfe-fdc1-4ec3-b874-c1093a456db1',
+    ),
+  );
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+  await DatadogSdk.runApp(configuration, TrackingConsent.granted, () async {
+    runApp(MyApp());
+  });
 }
 
 class MyApp extends StatelessWidget {
@@ -14,10 +27,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'The wall',
       debugShowCheckedModeBanner: false,
       home: AuthPage(),
+      navigatorObservers: [
+        DatadogNavigationObserver(datadogSdk: DatadogSdk.instance),
+      ],
     );
   }
 }
