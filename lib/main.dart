ca29f0f2a9c98431ecc508dbcd9d011ae34742d8
Shintaro Miyazaki <tudopassa427911@gmail.com>
2024-02-10 17:39:43 +0900

    delete Datadog

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,12 @@
-import 'package:datadog_flutter_plugin/datadog_flutter_plugin.dart';
 import 'package:flutter/material.dart';
 import 'package:the_wall/pages/auth_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-final configuration = DatadogConfiguration(
-  clientToken: 'pub3a63fb84a35c7fa2269d949b8ab4c706',
-  env: 'prod',
-  site: DatadogSite.ap1,
-  nativeCrashReportEnabled: true,
-  loggingConfiguration: DatadogLoggingConfiguration(),
-  rumConfiguration: DatadogRumConfiguration(
-    applicationId: '2505fdfe-fdc1-4ec3-b874-c1093a456db1',
-  ),
-);
-
 void main() async {
-  final configuration = DatadogConfiguration(
-    clientToken: 'pub3a63fb84a35c7fa2269d949b8ab4c706',
-    env: 'prod',
-    site: DatadogSite.ap1,
-    nativeCrashReportEnabled: true,
-    loggingConfiguration: DatadogLoggingConfiguration(),
-    rumConfiguration: DatadogRumConfiguration(
-      applicationId: '2505fdfe-fdc1-4ec3-b874-c1093a456db1',
-    ),
-  );
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  await DatadogSdk.runApp(configuration, TrackingConsent.granted, () async {
-    runApp(MyApp());
-  });
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -38,13 +14,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'The wall',
       debugShowCheckedModeBanner: false,
       home: AuthPage(),
-      navigatorObservers: [
-        DatadogNavigationObserver(datadogSdk: DatadogSdk.instance),
-      ],
     );
   }
 }
